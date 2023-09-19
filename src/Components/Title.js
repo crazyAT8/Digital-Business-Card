5f837a030bc6cf9daf12f8ee54bc22d30c05b270
crazyAT8 <88wwatson@gmail.com>
2023-09-18 21:52:37 -0500

    9/18/23

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,8 +4,8 @@ export default function Title() {
     return (
         <div className='title'>
             <h1 className='header name'>William Watson</h1>
-            <h4 className='header'>Job Title</h4>
-            <h5 className='header'>Company</h5>
+            <h4 className='header jobTitle'>Front-end Web Developer</h4>
+            <h5 className='header'></h5>
         </div>
     )
 }
